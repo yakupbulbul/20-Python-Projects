da7507fe92979ae0d46234f9edef5969c84c3e31
yakupbulbul <81179438+yakupbulbul@users.noreply.github.cod>
2022-11-09 17:52:10 +0300

    leap year

---
+++ leap_year_checker/python_app.py
@@ -0,0 +1,17 @@
+
+def is_leap_year(year): 
+    if year % 4 ==0: 
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("Leap year")
+            else:
+                print("Not Leap Year")
+           
+        else:
+            print("Leap year")
+    else:
+        print("Not Leap Year")
+
+
+year = int(input("Enter the year: "))
+is_leap_year(year)
