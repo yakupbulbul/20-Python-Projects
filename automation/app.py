cc2a636c8d38916f54023e3939b1962a555d58bd
yakupbulbul <81179438+yakupbulbul@users.noreply.github.cod>
2022-11-11 01:33:18 +0300

    automation

---
+++ automation/app.py
@@ -0,0 +1,22 @@
+import schedule
+import requests
+import time
+
+
+
+def send_message():
+    resp = requests.post(
+    "https://textbelt.com/text", {
+    "phone": "++++++++",
+    "message": "Hey, How are you",
+    'key': 'textbelt',
+    })
+    print(resp.json())
+
+
+#schedule.every().day.at("06:00").do(send_message)
+schedule.every(10).seconds.do(send_message)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
